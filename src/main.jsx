6a2ddc9e3e5a58922a4378455a5951577c165fb3
michealdev1 <dommytee6@gmail.com>
2026-07-31 22:40:06 +0630

    Add GitHub Pages deployment workflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init();
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
